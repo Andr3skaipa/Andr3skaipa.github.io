0a40e96b5abcb34f44ad83f427a047ca5fc755db
andrescaipa <andr3skaipa@gmail.com>
2022-11-22 16:14:29 -0500

    Data base connect

--- src/List.js
+++ src/List.js
@@ -1,27 +1,89 @@
 import './List.css';
-import { ListIntems } from "./utils/items-array";
 import { useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, child, get, update } from "firebase/database";
 
 export default function WishList() {
-  const [checkedState, setCheckedState] = useState(
-    new Array(ListIntems.length).fill(false)
-  );
-  const [agregados, setAgregados] = useState([]);
+  const [ListItems, setListItems] = useState([]);
+  const firebaseConfig = {
+    apiKey: "AIzaSyDs_CICOO1ymVmLyo5Q8zT6SukzmY8cvus",
+    authDomain: "db-agustin-baby-wish-list.firebaseapp.com",
+    projectId: "db-agustin-baby-wish-list",
+    storageBucket: "db-agustin-baby-wish-list.appspot.com",
+    messagingSenderId: "675708194625",
+    appId: "1:675708194625:web:1c5860b9e8cc06ff909bc0"
+  };
+  const app = initializeApp(firebaseConfig);
+  const dbRef = ref(getDatabase(app));
+
+  if (ListItems.length === 0) {
+    get(child(dbRef, `items/`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        setListItems(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
   const [option, setOptions] = useState(true);
+  const [checkedState, setCheckedState] = useState(
+    new Array(ListItems.length).fill(false)
+  );
+
+  ListItems.map((item, index) => {
+    checkedState[index] = item.checked;
+  });
+
   const handleOnChange = (position) => {
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item
     );
     setCheckedState(updatedCheckedState);
-    setAgregados(agregados.concat(ListIntems[position]));
+    const newList = ListItems;
+    newList[position].checked = !newList[position].checked;
+    setListItems(newList);
   };
 
   const handleButtonOnChange = () => {
     setOptions(!option);
+    console.log(checkedState);
   }
 
   const handleFinalizar = () => {
-    console.log("Hola mundo");
+    const newList = ListItems
+    console.log(datos.name);
+    newList.forEach(item => 
+      item.checked ? item.invitado = datos.name : ""
+    );
+    console.log(newList);
+    const db = getDatabase();
+    newList.forEach((item, index) =>
+    {
+      update(ref(db, 'items/' + index), item)
+     .then(() => {
+       console.log('Data saved successfully!');
+     })
+     .catch((error) => {
+       console.log('The write failed...');
+     });
+    }
+    );
+  }
+
+  // Manejo Invitado
+
+  const [datos, setDatos] = useState({
+    name: ''
+  })
+
+  const handleInputChange = (event) => {
+    setDatos({
+      ...datos,
+      [event.target.name]: event.target.value
+    })
   }
 
   return (
@@ -37,11 +99,11 @@ export default function WishList() {
           {option ?
             <div className="checkbox-list">
               <p className='List-title'>Opciones</p>
-              <ul className="ListIntems-list">
-                {ListIntems.map(({ name }, index) => {
+              <ul className="ListItems-list">
+                {ListItems.map(({ name, checked }, index) => {
                   return (
                     <li key={index}>
-                      <div className="ListIntems-list-item">
+                      <div className="ListItems-list-item">
                         <div className="left-section">
                           <input
                             type="checkbox"
@@ -64,32 +126,30 @@ export default function WishList() {
             :
             <div className="checkbox-list">
               <p className='List-title'>Seleccion</p>
-              <ul className="ListIntems-list">
-                {ListIntems.map(({ name }, index) => {
-                  if (checkedState[index]) {
-                    return (
-                      <li key={index}>
-                        <div className="ListIntems-list-item">
-                          <div className="left-section">
-                            <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
-                          </div>
+              <ul className="ListItems-list">
+                {ListItems.map(({ name, checked }, index) => {
+                  return checked ? (
+                    <li key={index}>
+                      <div className="ListItems-list-item">
+                        <div className="left-section">
+                          <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
                         </div>
-                      </li>
-                    );
-                  }
+                      </div>
+                    </li>
+                  ) : null;
                 })}
                 <li>
                 </li>
               </ul>
               <label className='nombre-invitado'>
                 Nombre invitado
-                <input type="text" name="name" className='input-invitado'/>
+                <input type="text" placeholder="Nombre" onChange={handleInputChange} name="name" className='input-invitado' />
               </label>
               <div className='button-div'>
-              <button className="button-6" onClick={() => handleButtonOnChange()}>Editar</button>
-              <button className="button-6" onClick={() => handleFinalizar()}>Confirmar</button>
+                <button className="button-6" onClick={() => handleButtonOnChange()}>Editar</button>
+                <button className="button-6" onClick={() => handleFinalizar()}>Confirmar</button>
               </div>
-              
+
             </div>
           }
         </div>
--- src/utils/items-array.js
+++ src/utils/items-array.js
@@ -1,59 +1,173 @@
 export const ListIntems = [
-    { name: "Bolso Pañalera"},
-    { name: "Lima eléctrica para uños"},
-    { name: "Extractor de leche entrada doble electricos"},
-    { name: "Cama cuna"},
-    { name: "Colecho o Moisés"},
-    { name: "Pañales Huggies etapa 0 / 5"},
-    { name: "Pañitos húmedos"},
-    { name: "Tetero marca avent"},
-    { name: "Tetero marca avent"},
-    { name: "Tetero marca avent"},
-    { name: "Chupa tetero marca avent"},
-    { name: "Chupa tetero marca avent"},
-    { name: "Chupa tetero marca avent"},
-    { name: "Chupo"},
-    { name: "Monitor con cámara bebé"},
-    { name: "Almohada recién nacidos"},
-    { name: "Calentador de teteros"},
-    { name: "Cojín de lactancia ( x )"},
-    { name: "Tapete de fichas acolchado"},
-    { name: "Paquete de bolas piscina"},
-    { name: "Crema desitin"},
-    { name: "Sonajero"},
-    { name: "Móvil"},
-    { name: "Rascaencia"},
-    { name: "Cepillo de seda para peinarlo"},
-    { name: "Loción para bebé"},
-    { name: "Shampoo para bebé"},
-    { name: "Jabón líquido"},
-    { name: "Crema para bebé"},
-    { name: "Babero plástico"},
-    { name: "Babero tela"},
-    { name: "Casco gateador"},
-    { name: "Rodilleras"},
-    { name: "Bolsas para banco de leche"},
-    { name: "Bolsas para banco de leche"},
-    { name: "Bolsas para banco de leche"},
-    { name: "Saco para dormir bby fresh"},
-    { name: "Cobija"},
-    { name: "Cambiador portátil"},
-    { name: "Sácamocos"},
-    { name: "Piscina inflable"},
-    { name: "Saco para dormir"},
-    { name: "Toallas"},
-    { name: "jaula patio de juegos portatil"},
-    { name: "Lámpara musical"},
-    { name: "Alfombra de entretenimiento"},
-    { name: "Gorritos"},
-    { name: "Medias"},
-    { name: "Mitones"},
-    { name: "Ropa"},
-    { name: "Termómetro para agua"},
-    { name: "Juguetes"},
-    { name: "Papelera/basura pañales"},
-    { name: "Juegues de estimulación temprana"},
-    { name: "Vajilla para bebé anti reguero"},
-    { name: "Espejo retrovisor para el carro"},
-    { name: "Horimiya"},
+    { name: "Bolso Pañalera",
+      invitado: null
+    },
+    { name: "Lima eléctrica para uños",
+      invitado: null
+    },
+    { name: "Extractor de leche entrada doble electricos",
+      invitado: null
+    },
+    { name: "Cama cuna",
+      invitado: null
+    },
+    { name: "Colecho o Moisés",
+      invitado: null
+    },
+    { name: "Pañales Huggies etapa 0 / 5",
+      invitado: null
+    },
+    { name: "Pañitos húmedos",
+      invitado: null
+    },
+    { name: "Tetero marca avent",
+      invitado: null
+    },
+    { name: "Tetero marca avent",
+      invitado: null
+    },
+    { name: "Tetero marca avent",
+      invitado: null
+    },
+    { name: "Chupa tetero marca avent",
+      invitado: null
+    },
+    { name: "Chupa tetero marca avent",
+      invitado: null
+    },
+    { name: "Chupa tetero marca avent",
+      invitado: null
+    },
+    { name: "Chupo",
+      invitado: null
+    },
+    { name: "Monitor con cámara bebé",
+      invitado: null
+    },
+    { name: "Almohada recién nacidos",
+      invitado: null
+    },
+    { name: "Calentador de teteros",
+      invitado: null
+    },
+    { name: "Cojín de lactancia ( x )",
+      invitado: null
+    },
+    { name: "Tapete de fichas acolchado",
+      invitado: null
+    },
+    { name: "Paquete de bolas piscina",
+      invitado: null
+    },
+    { name: "Crema desitin",
+      invitado: null
+    },
+    { name: "Sonajero",
+      invitado: null
+    },
+    { name: "Móvil",
+      invitado: null
+    },
+    { name: "Rascaencia",
+      invitado: null
+    },
+    { name: "Cepillo de seda para peinarlo",
+      invitado: null
+    },
+    { name: "Loción para bebé",
+      invitado: null
+    },
+    { name: "Shampoo para bebé",
+      invitado: null
+    },
+    { name: "Jabón líquido",
+      invitado: null
+    },
+    { name: "Crema para bebé",
+      invitado: null
+    },
+    { name: "Babero plástico",
+      invitado: null
+    },
+    { name: "Babero tela",
+      invitado: null
+    },
+    { name: "Casco gateador",
+      invitado: null
+    },
+    { name: "Rodilleras",
+      invitado: null
+    },
+    { name: "Bolsas para banco de leche",
+      invitado: null
+    },
+    { name: "Bolsas para banco de leche",
+      invitado: null
+    },
+    { name: "Bolsas para banco de leche",
+      invitado: null
+    },
+    { name: "Saco para dormir bby fresh",
+      invitado: null
+    },
+    { name: "Cobija",
+      invitado: null
+    },
+    { name: "Cambiador portátil",
+      invitado: null
+    },
+    { name: "Sácamocos",
+      invitado: null
+    },
+    { name: "Piscina inflable",
+      invitado: null
+    },
+    { name: "Saco para dormir",
+      invitado: null
+    },
+    { name: "Toallas",
+      invitado: null
+    },
+    { name: "jaula patio de juegos portatil",
+      invitado: null
+    },
+    { name: "Lámpara musical",
+      invitado: null
+    },
+    { name: "Alfombra de entretenimiento",
+      invitado: null
+    },
+    { name: "Gorritos",
+      invitado: null
+    },
+    { name: "Medias",
+      invitado: null
+    },
+    { name: "Mitones",
+      invitado: null
+    },
+    { name: "Ropa",
+      invitado: null
+    },
+    { name: "Termómetro para agua",
+      invitado: null
+    },
+    { name: "Juguetes",
+      invitado: null
+    },
+    { name: "Papelera/basura pañales",
+      invitado: null
+    },
+    { name: "Juegues de estimulación temprana",
+      invitado: null
+    },
+    { name: "Vajilla para bebé anti reguero",
+      invitado: null
+    },
+    { name: "Espejo retrovisor para el carro",
+      invitado: null
+    },
+    { name: "Horimiya",
+      invitado: null
+    },
     ];
